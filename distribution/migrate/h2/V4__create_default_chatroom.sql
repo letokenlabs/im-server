a91faac95dd6bed95a79d9a5b7b866174a2e145f
letokenlabs <letokenlabs@gmail.com>
2023-02-24 15:54:01 +0800

    fork init

---
+++ distribution/migrate/h2/V4__create_default_chatroom.sql
@@ -0,0 +1,6 @@
+
+insert into t_chatroom (`_cid`, `_title`, `_desc`,`_dt`) values ('chatroom1','聊天室1','测试聊天室1，用来演示聊天室功能', 1);
+
+insert into t_chatroom (`_cid`, `_title`, `_desc`,`_dt`) values ('chatroom2','聊天室2','测试聊天室2，用来演示聊天室功能', 1);
+
+insert into t_chatroom (`_cid`, `_title`, `_desc`,`_dt`) values ('chatroom3','聊天室3','测试聊天室3，用来演示聊天室功能', 1);
